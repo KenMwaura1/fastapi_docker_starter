715adc457ef0147894f039ad7cdd1037c645e60f
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-09-23 14:47:55 +0300

    Updated the session file and added psycopg2

--- app/db/session.py
+++ app/db/session.py
@@ -1,5 +1,12 @@
-import SQLALCHEMY as SQLALCHEMY
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 
-SQLALCHEMY_DATABASE_URI = ''
+load_dotenv()
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
+
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
