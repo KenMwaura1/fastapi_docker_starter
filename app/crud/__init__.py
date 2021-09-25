22c264ed3929f31708d0b06ed0c204f62b1604e1
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-09-25 12:15:58 +0300

    added the app configuration

---
+++ app/crud/__init__.py
@@ -0,0 +1,1 @@
+from .crud_anime import anime
--- run.sh
+++ run.sh
@@ -4,4 +4,7 @@ export APP_MODULE=${APP_MODULE-app.main:app}
 export HOST=${HOST:-0.0.0.0}
 export PORT=${PORT:-8001}
 
-exec uvicorn --reload --host $HOST --port $PORT "$APP_MODULE"
+# Create initial data in DB
+python ./initial_data.py
+
+exec uvicorn --reload --host "$HOST" --port "$PORT" "$APP_MODULE"
