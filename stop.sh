492599749d9391d9529625cbce51e098bd1b25bc
KenMwaura1 <kemwaura@gmail.com>
2021-12-16 02:59:16 +0300

    updated workflow files

--- stop.sh
+++ stop.sh
@@ -1,12 +1,17 @@
 #  shell script to exit the program
 
-
 export APP_MODULE=${APP_MODULE-app.main:app}
 export HOST=${HOST:-0.0.0.0}
 export PORT=${PORT:-8001}
 
+# Create initial data in DB
+python ./initial_data.py
 
-sleep 10
+
+# Run server
+exec uvicorn --reload --host "$HOST" --port "$PORT" "$APP_MODULE" &
+
+sleep 15
 
 # LASTPID=$!
 
