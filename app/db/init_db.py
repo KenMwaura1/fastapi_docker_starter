bf06e84f57c1bfa122e7ff460add1133b6ea9562
KenMwaura1 <kemwaura@gmail.com>
2023-01-28 12:50:30 +0300

    fixed image url

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -13,16 +13,20 @@ def init_db(db: Session) -> None:  # sourcery skip: merge-nested-ifs
     Base.metadata.create_all(bind=engine)
     test_anime = crud.anime.get_multi(SessionLocal())
     # print(test_anime)
-    print(anime_data.get_anime()[0])
+    print('test')
+    #print(anime_data.get_anime())
+    top_anime = anime_data.get_top_anime()
+    all_anime = anime_data.get_anime()
+    search_anime = anime_data.search_anime_by_name('naruto')
     if not test_anime:
-        for anime in anime_data.get_anime():
+        for anime in top_anime:
             if test_anime is not anime.get('mal_id'):
 
                 new_anime = schema.AnimeCreate(
                     mal_id=anime.get('mal_id'),
                     url=anime.get('url'),
                     title=anime.get('title'),
-                    image_url=anime.get('image_url'),
+                    image_url=anime.get('images').get('webp').get('large_image_url'),
                     synopsis=anime.get('synopsis'),
                     type=anime.get('type'),
                     airing_start=anime.get('airing_start'),
