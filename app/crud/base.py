4ec714add626d51303848ba46b96ef1be24992f0
KenMwaura1 <kemwaura@gmail.com>
2025-02-02 12:00:56 +0300

    Refactor CRUDBase class to support update operations and improve object handling

--- app/crud/base.py
+++ app/crud/base.py
@@ -1,18 +1,17 @@
-from typing import Any, Dict, Generic, List, Optional, Type, TypeVar
+from typing import Any, Generic, List, Optional, Type, TypeVar
 
-from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
 from app.db.base_class import Base
 
-ModelType = TypeVar("ModelType", bound=Base)
+ModelType = TypeVar("ModelType", bound=BaseModel)
 CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
 UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
 
 
-class CRUDBase(Generic[ModelType, CreateSchemaType]):  # 1
-    def __init__(self, model: Type[ModelType]):  # 2
+class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
+    def __init__(self, model: Type[ModelType]):
         """
         CRUD object with default methods to Create, Read, Update, Delete (CRUD).
         **Parameters**
@@ -23,12 +22,13 @@ class CRUDBase(Generic[ModelType, CreateSchemaType]):  # 1
         self.model = model
 
     def get(self, db: Session, id: Any) -> Optional[ModelType]:
-        return db.query(self.model).filter(self.model.mal_id == id).first()  # 3
+        return db.query(self.model).filter(self.model.mal_id == id).first()
 
     def get_multi(
             self, db: Session, *, skip: int = 0, limit: int = 100
     ) -> List[ModelType]:
-        return db.query(self.model).offset(skip).limit(limit).all()  # 4
+        return db.query(self.model).offset(skip).limit(limit).all()
+
     def get_by_id(
             self, db: Session, id: Any
     ) -> List[ModelType]:
@@ -43,23 +43,19 @@ class CRUDBase(Generic[ModelType, CreateSchemaType]):  # 1
             self, db: Session, *, skip: int = 0, limit: int = 100, genre: str
     ) -> List[ModelType]:
         return db.query(self.model).filter(self.model.genres == genre).offset(skip).limit(limit).all()
-    
-
 
     def create(self, db: Session, *, obj_in: CreateSchemaType) -> ModelType:
-        obj_in_data = jsonable_encoder(obj_in)
+        obj_in_data = obj_in.dict()
         db_obj = self.model(**obj_in_data)  # type: ignore
         db.add(db_obj)
-        db.commit()  # 5
+        db.commit()
         db.refresh(db_obj)
         return db_obj
-    # TODO: Add methods to update and remove from the db
+
     def update(self, db: Session, *, db_obj: ModelType, obj_in: UpdateSchemaType) -> ModelType:
-        obj_data = jsonable_encoder(db_obj)
-        update_data = obj_in.dict(skip_defaults=True)
-        for field in obj_data:
-            if field in update_data:
-                setattr(db_obj, field, update_data[field])
+        update_data = obj_in.dict(exclude_unset=True)
+        for field in update_data:
+            setattr(db_obj, field, update_data[field])
         db.add(db_obj)
         db.commit()
         db.refresh(db_obj)
