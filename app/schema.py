350f913ae361359d024cb8222b7ce961868f8338
KenMwaura1 <kemwaura@gmail.com>
2025-02-02 12:44:30 +0300

    Make airing_start optional in AnimeBaseModel and update Config for ORM compatibility

--- app/schema.py
+++ app/schema.py
@@ -1,4 +1,6 @@
+from typing import Optional
 from pydantic import BaseModel, HttpUrl
+
 class AnimeBaseModel(BaseModel):
     mal_id: int
     url: HttpUrl
@@ -6,28 +8,20 @@ class AnimeBaseModel(BaseModel):
     image_url: str
     synopsis: str
     type: str
-    airing_start: str
+    airing_start: Optional[str]  # Make airing_start optional
     episodes: int
     members: int
 
-
-# Properties shared by models stored in DB
 class AnimeInBase(AnimeBaseModel):
     id: int
     class Config:
-        orm_mode = True
+        from_attributes = True
 
-
-# Properties to return to client
 class Anime(AnimeInBase):
     pass
 
-
-# Properties to create new anime
 class AnimeCreate(AnimeBaseModel):
     pass
 
-
-# Properties to update existing anime
 class AnimeUpdate(AnimeBaseModel):
     pass
