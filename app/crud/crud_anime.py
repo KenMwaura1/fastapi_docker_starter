8f82878c953fc9948864a71ebf966df835f1e314
KenMwaura1 <kemwaura@gmail.com>
2025-02-02 12:44:40 +0300

    Update CRUDAnime to use AnimeModel for improved type consistency

--- app/crud/crud_anime.py
+++ app/crud/crud_anime.py
@@ -1,11 +1,12 @@
 from typing import Type
 from app.crud.base import CRUDBase
+from app.models.anime import Anime as AnimeModel
 from app.schema import Anime, AnimeCreate, AnimeUpdate
 
-class CRUDAnime(CRUDBase[Anime, AnimeCreate, AnimeUpdate]):
-    def __init__(self, model: Type[Anime]):
+class CRUDAnime(CRUDBase[AnimeModel, AnimeCreate, AnimeUpdate]):
+    def __init__(self, model: Type[AnimeModel]):
         super().__init__(model)
 
     # Define other CRUD methods here
 
-anime = CRUDAnime(Anime)
+anime = CRUDAnime(AnimeModel)
