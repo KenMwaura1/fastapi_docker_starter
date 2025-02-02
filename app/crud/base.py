3019df76cefc0d573436fe001ee3174e1c4f378a
KenMwaura1 <kemwaura@gmail.com>
2025-02-02 12:44:35 +0300

    Change ModelType type variable bound from BaseModel to Base for improved type consistency

--- app/crud/base.py
+++ app/crud/base.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 
 from app.db.base_class import Base
 
-ModelType = TypeVar("ModelType", bound=BaseModel)
+ModelType = TypeVar("ModelType", bound=Base)
 CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
 UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
 
