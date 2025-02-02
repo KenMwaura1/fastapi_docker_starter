dd8b5bee3c2994e2d8fc9dc5fe0b553f58cb4389
KenMwaura1 <kemwaura@gmail.com>
2025-02-02 12:44:25 +0300

    Refactor anime data retrieval functions to streamline API calls and remove unused code

--- app/anime_data.py
+++ app/anime_data.py
@@ -1,92 +1,41 @@
 from typing import Any, Dict
 import asyncio
-from jikanpy import AioJikan, Jikan
+from jikanpy import Jikan
 
 jikan = Jikan()
 
-
 def get_anime():
-    sn = jikan.seasons(extension='now')
-    data = sn.get('data')
-    print("Here")
-    print(data)
-    return data
+    response = jikan.search('anime',query='', page=1)
+    return response['data']
 
 def get_top_anime():
-    sn = jikan.top(type='anime')
-    data = sn.get('data')
-    print("Here")
-    print(data)
-    return data
+    response = jikan.top(type='anime', page=2)
+    return response['data']
 
 def get_anime_by_id(id):
-    sn = jikan.anime(id)
-    data = sn
-    print(data)
-    return data
+    response = jikan.anime(id)
+    return response
 
 def get_anime_by_name(name):
-    sn = jikan.search('anime', name)
-    data = sn.get('results')
-    print(data)
-    return data
+    response = jikan.search(type='anime', query=name, page=1)
+    return response['data']
 
-def get_anime_by_genre(genre):
-    sn = jikan.genres(type='anime', filter=genre)
-    data = sn.get('data')
-    print(data)
-    return data
+def get_anime_by_genre(genre_id):
+    response = jikan.genre(type='anime', genre_id=genre_id)
+    return response['data']
 
 def get_anime_by_season(year, season):
-    sn = jikan.seasons(year=year,
-        season='winter',
-        # page=2,
-        parameters={'filter': 'tv'})
-    data = sn.get('data')
-    print(data)
-    return data
+    response = jikan.season(year=year, season=season)
+    return response['data']
 
 def get_anime_by_year(year):
-    sn = jikan.seasons(year=year)
-    data = sn.get('data')
-    print(data)
-    return data
+    response = jikan.season_archive(year=year)
+    return response['data']
 
-def get_anime_by_producer(producer):
-    sn = jikan.producer(producer)
-    data = sn.get('data')
-    print(data)
-    return data
+def get_anime_by_producer(producer_id):
+    response = jikan.producer(producer_id)
+    return response['data']
 
-def search_anime_by_name(name):
-    sn = jikan.search('anime', name)
-    data = sn.get('data')
-    print(data)
-    return data
-# search_anime_by_name('jojo')
-# print(get_top_anime()[0].get('images').get('webp').get('large_image_url'))
-
-
-"""
-async def main() -> Any:
-    async with AioJikan() as aiojikan:
-        latest_season: Any = await aiojikan.season()
-        print(latest_season.get('anime')[0])
-        print("here")
-        return await latest_season
-
-
-# asyncio.run(main())
-# fn = main()
-loop = asyncio.get_event_loop()
-
-
-async def get_anime():
-    fn = asyncio.run(main())
-    t1 = main()
-    await fn
-    print(t1)
-    return await t1
-
-m = asyncio.wait(get_anime())
-print(m)"""
+def search_anime_by_name(name: str):
+    response = jikan.search('anime', query=name, page=1)
+    return response['data']
